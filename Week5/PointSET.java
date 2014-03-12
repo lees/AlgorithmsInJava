7bee74e014d108ae719ee04a8b3d04235c0cc79d
Lees <leesasp@gmail.com>
2014-03-12 21:03:30 +0400

    First code

--- Week5/PointSET.java
+++ Week5/PointSET.java
@@ -1,10 +1,65 @@
+
+import java.util.TreeSet;
+import java.util.Queue;
+
 public class PointSET {
-   public PointSET()                               // construct an empty set of points
-   public boolean isEmpty()                        // is the set empty?
-   public int size()                               // number of points in the set
-   public void insert(Point2D p)                   // add the point p to the set (if it is not already in the set)
-   public boolean contains(Point2D p)              // does the set contain the point p?
-   public void draw()                              // draw all of the points to standard draw
-   public Iterable<Point2D> range(RectHV rect)     // all points in the set that are inside the rectangle
-   public Point2D nearest(Point2D p)               // a nearest neighbor in the set to p; null if set is empty
+
+    private TreeSet<Point2D> tree;
+
+    public PointSET()                               // construct an empty set of points
+    {
+       tree = new TreeSet<Point2D>();
+    }
+
+    public boolean isEmpty()                        // is the set empty?
+    {
+       return tree.isEmpty();
+    }
+
+    public int size()                               // number of points in the set
+    {
+       return tree.size();
+    }
+
+
+    public void insert(Point2D p)                   // add the point p to the set (if it is not already in the set)
+    {
+        tree.add(p);
+    }
+
+    public boolean contains(Point2D p)              // does the set contain the point p?
+    {
+        return tree.contains(p);
+    }
+
+    public void draw()                              // draw all of the points to standard draw
+    {
+        for (Point2D p : tree)
+            StdDraw.point(p.getX(),p.getY());
+    }
+
+    public Iterable<Point2D> range(RectHV rect)     // all points in the set that are inside the rectangle
+    {
+        Queue<Point2D> queue = new Queue<Point2D>();
+        for (Point2D p: tree)
+            if (rect.contains(p))
+                queue.add(p);
+        return queue;
+    }
+
+    public Point2D nearest(Point2D p)               // a nearest neighbor in the set to p; null if set is empty
+    {
+        Point2D closestPoint = tree.first();
+        double closestDistance = p.distance(currentPoint);
+        for (Point2D point: tree)
+        {
+            double distance = point.distance(p);
+            if (distance < closestDistance)
+            {
+                closestPoint = point;
+                closestDistance = distance;
+            }
+        }
+        return closestPoint;
+    }
 }
